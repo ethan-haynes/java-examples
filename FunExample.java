40f05e0c6dd9f0e8f29fe43873325f7f15bd3fa0
Ethan Haynes <ethanhaynes@Ethans-MacBook-Pro.local>
2017-04-12 23:48:53 -0500

    first commit

---
+++ FunExample.java
@@ -0,0 +1,18 @@
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+class FunExample {
+  public static void main(String[] args) {
+    /* This is an example of an iife in java */
+    ((Consumer<Integer>) y ->
+      System.out.println(y + y)
+    ).accept(1234);
+
+    /* This is an example of an iife with a return value in java */
+    System.out.println(
+      ((Function<Integer,Void>) y ->
+        y + y
+      ).apply(2)
+    );
+  }
+}
